401c3173378f964dbd066a6729172e35135d1059
Rafael Vieira <rafaelvieirapalmital@outlook.com>
2021-10-24 11:45:38 -0300

    Adiiconando scroll suave na pagina e adicionando jquery internamemte

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/////////////// VALIDAÇÂO FORMULÁRIO //////////////
+
+/////////////// MENU MOBILE /////////////
+document.getElementsByClassName("menu-mobile")[0].addEventListener("click", ()=>{
+    document.getElementsByClassName("nav")[0].classList.add("abrir-menu");
+})
+
+document.getElementsByClassName("fechar-menu")[0].addEventListener("click", ()=>{
+    document.getElementsByClassName("nav")[0].classList.remove("abrir-menu");
+})
+
+document.getElementsByClassName("nav")[0].addEventListener("click", ()=>{
+    document.getElementsByClassName("nav")[0].classList.remove("abrir-menu");
+})
+
+/////////////// CARROSSEL ///////////////
+$('.carrossel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+
+////////////// SCROLL SUAVE NOS LINKS INTERNOS ////////////
+$('.nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 0
+    }, 500);
+});
+
+$('.btn-topo a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 125
+    }, 500);
+});
