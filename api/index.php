b72f5dd9b69019d2182aec898f9ad05323a2210d
Rafael Vieira <rafaelvieirapalmital@outlook.com>
2021-10-30 10:11:07 -0300

    Alterando estilo do input de validacao e o arquivo index.php

--- api/index.php
+++ api/index.php
@@ -1,30 +1,49 @@
 <?php
 
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $telefone = $_POST['telefone'];
-    $menssagem = $_POST['menssagem'];
+    $destino = 'rafaelvieirapalmital@outlook.com';
+    $assunto = 'Mensagem enviada através do site de portifólio';
+
+    $dados = $_POST;
+
+    $nome = $dados['nome'];
+    $email = $dados['email'];
+    $telefone = $dados['telefone'];
+    $menssagem = $dados['menssagem'];
 
     $data = date('d/m/y');
     $hora = date('H:I:S');
 
     $corpo_emmail = "
-        <html>
-            <p><b>Nome: {$nome}</b></p>
-            <p><b>E-mail: {$email}</b></p>
-            <p><b>Telefone: {$telefone}</b></p>
-            <p><b>Mensagem: {$menssagem}</b></p>
+        <style>
+            div{
+                width: 98%,
+                background: #000,
+                border: 2px solid #F90446;
+                border-radius: 3px;
+            }
 
-            <p>Esse E-mail foi enviado no dia {$data} às {$hora}.</p>
+            div > p{
+                color: #F90446;
+                font-size: 1em;
+            }
+        </style>
+        <html>
+            <div>
+                <p><b>Nome: {$nome}</b></p>
+                <p><b>E-mail: {$email}</b></p>
+                <p><b>Telefone: {$telefone}</b></p>
+                <p><b>Mensagem: {$menssagem}</b></p>
+
+                <p>Esse E-mail foi enviado no dia {$data} às {$hora}.</p>
+            </div>
         <html>";
 
-    $destino = 'rafaelvieirapalmital@outlook.com';
-    $assunto = 'Mensagem enviada através do site de portifólio';
+    $headers =  "Content-Type:text/html; charset=UTF-8\n";
+    $headers .= "De: {$nome}, Para: <{$destino}>";
+    $headers .= "X-Sender:  <sistema@dominio.com.br>\n";
+    $headers .= "X-Mailer: PHP  v".phpversion()."\n";
+    $headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+    $headers .= "Return-Path:  <sistema@dominio.com.br>\n";
+    $headers .= "MIME-Version: 1.0\n";
 
-    $headers = "MIME-Version: 1.0\n";
-    $headers += "Content-type: text/html; charset=iso-8859-1\n";
-    $headers += "From: {$nom3} <{$destino}>";
-
-    email($destino, $assunto, $corpo_emmail, $headers);
-
-    echo '<meta http-equiv="refresh" content="10; URL="../">';
+    mail($destino, $assunto, $corpo_emmail, $headers);
