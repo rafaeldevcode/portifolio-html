779b8f2067dcfcd249586acb3e8ea8a2b632c4b8
Rafael Vieira <rafaelvieirapalmital@outlook.com>
2021-12-10 07:17:54 -0300

    Correcao ao exibir imagem ao clicar

--- js/main.js
+++ js/main.js
@@ -222,7 +222,7 @@ for(let i = 0; i < zoom.length; i++){
             div.setAttribute('class', 'fechar-image');
         
         let img = document.createElement('img');
-            img.setAttribute('src', `./images/original/${src}`)
+            img.setAttribute('src', src)
             img.setAttribute('alt', alt);
 
             section.appendChild(div);
